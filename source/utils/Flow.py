4c5645a54e5f61e85a663bc0be2b3d6848c75b7a
Rashid Bekhbudov <40560297+bittenradish@users.noreply.github.com>
2026-02-05 09:30:25 +0100

    Add box covering pipeline functions and JSON handling

--- source/utils/Flow.py
+++ source/utils/Flow.py
@@ -1,13 +1,22 @@
 import graph_tool.all as gt
 import numpy as np
 import os
+import json
+import glob
+import subprocess
+import shlex
 
 __all__ = ['analysis_folder', 
            'is_weight_type_supported', 
            'has_parallel_edges', 
            'simplify_graph_with_weights', 
            'prepare_folder',
-           'directed_to_undirected_sum_weights'
+           'directed_to_undirected_sum_weights',
+           'saveTSV',
+           'get_latest_output_json',
+           'save_json',
+           'run_box_covering',
+           'start_box_covering_pipeline'
            ]
 
 # Flow Variables
@@ -15,6 +24,65 @@ __all__ = ['analysis_folder',
 analysis_folder = "./analysis_data/"
 
 # Flow functions
+
+## Box covering pipeline functions
+
+
+def saveTSV(filePath, graph):
+    # fmt='%d' ensures ids are written as integers
+    np.savetxt(filePath, graph.get_edges()[:, :2], fmt='%d', delimiter='\t')
+
+
+def get_latest_output_json():
+    list_of_files = glob.glob('./graph_sketch_fractality/jlog/*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+
+    f = open(latest_file, "r").read()
+    json_file = json.loads(f)
+
+    return json_file
+
+
+def save_json(path, json_file):
+    with open(path, "w") as outfile:
+        json.dump(json_file, outfile)
+
+
+def run_box_covering(**kwargs):
+    
+    # Default arguments of the method:
+    default_args = {'type': 'gen', 'graph': "flower 1000 1 2", 'method': 'sketch', 'alpha': '1', 'least_coverage': '1',
+           'sketch_k': '128', 'multipass': '10000', 'rad_min': '1', 'rad_max': '30', 'random_seed': '114514'}
+    
+    options = ""
+    
+    for key in kwargs:
+        default_args[key] = kwargs[key]
+        
+    if default_args['type'] == 'gen':
+        a = default_args['graph']
+        default_args['graph'] = f'"{a}"'
+            
+    for key in default_args:
+        options += f" -{key}={default_args[key]}"
+        
+    subprocess.run(shlex.split(f"./bin/box_cover {options}"), cwd='./graph_sketch_fractality')
+
+
+def start_box_covering_pipeline(graph, folder_path, is_mst):
+    suffix = "mst" if is_mst else "graph"
+
+    tsv_path = f"{folder_path}/{suffix}.tsv"
+    saveTSV(tsv_path, graph)
+
+    run_box_covering(type="tsv", graph=f".{tsv_path}")
+
+    json = get_latest_output_json()
+    save_json(f"{folder_path}/box_{suffix}_result.json", json)
+
+
+## Graph preparation functions
+
 
 def prepare_folder(graph_name):
     safe_graph_name = graph_name.replace('/', '__')
@@ -98,5 +166,4 @@ def directed_to_undirected_sum_weights(
     graph_result, result_map = simplify_graph_with_weights(g_undirected, weight_name=existing_weight_name, inplace=True)
     
     return graph_result, result_map
-
 
